b995ad3f8b2a3e50ebf30e5d3ee045b24d6d1d75
Abdelrahman <abdelrahman12312@outlook.com>
2025-05-22 19:24:11 +0300

    adding javascript fundamentals to github

---
+++ hello.js
@@ -0,0 +1,46 @@
+// const name = "Abdelrahman";
+// const age = 25;
+// const isStudent = false;
+// const address = { country: "Egypt", city: "Alexandrias" };
+// const hobbies = ["videogames", "Movies", "Coding"];
+// let undefinedVariable;
+// const nullVariable = null;
+// console.log(name, typeof name);
+// console.log(age, typeof age);
+// console.log(isStudent, typeof isStudent);
+// console.log(address, typeof address);
+// console.log(hobbies, typeof hobbies);
+// console.log(undefinedVariable, typeof undefinedVariable);
+// console.log(nullVariable, typeof nullVariable);
+// console.log(typeof null);
+
+// const userName = prompt("Enter your Name:");
+// const userAge = Number(prompt("Enter your Age:"));
+
+// let currentYear = new Date().getFullYear();
+// yearOfBirth = currentYear - userAge;
+// alert(
+//   `your Name is ${userName} your age is ${userAge} you were born in ${yearOfBirth}`
+// );
+
+// conditional logic practice
+
+// let number = 1;
+// if (number > 0) {
+//   console.log(`${number} is Positive`);
+// } else if (number < 0) {
+//   console.log(`${number} is Negaive`);
+// } else {
+//   console.log(`${number} is Zero`);
+// }
+
+// for (i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+result = sum(2, 3);
+console.log(result);
