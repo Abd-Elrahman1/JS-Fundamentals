4c874b0ae691099be3982c53dd176ccd08edaab5
Abdelrahman <abdelrahman12312@outlook.com>
2025-05-22 20:07:38 +0300

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2); // Remove the first two default elements
 
 if (args[0] === undefined) {
-  console.log("no argument");
+  console.log("No argument");
 } else {
   console.log(args[0]);
 }
