f3cb3e156d7fdf7619ef23dd34db2a1aac036d62
Abdelrahman <abdelrahman12312@outlook.com>
2025-05-22 20:22:48 +0300

    Create 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,7 @@
+const args = process.argv.slice(2); // Remove the first two default elements
+
+if (Number(args[0])) {
+  console.log(`My number: ${Number(args)}`);
+} else {
+  console.log("Not a number");
+}
